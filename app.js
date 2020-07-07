c06e0fdc6b0a090254ec8ee343a95346db61f5d2
plkujaw <jawor.jakub@gmail.com>
2020-07-07 14:23:25 +0100

    select items

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // ****** SELECT ITEMS **********
-
+const alert = document.querySelector(".alert");
+const todoInputForm = document.querySelector(".todo-form");
+const todoItemInput = document.getElementById("todo");
+const submitBtn = document.querySelector(".submit-btn");
+const todoContainer = document.querySelector(".todo-container");
+const todoList = document.querySelector(".todo-list");
+const clearBtn = document.querySelector(".clear-btn");
 // edit option
+let editItem;
+let editFlag = false;
+let editId = "";
 
 // ****** EVENT LISTENERS **********
 
